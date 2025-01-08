97f742ee43a37d254e0b99480eef578758c66c0a
Chris Reyes <Chris.Reyes@red-gate.com>
2025-01-07 17:44:08 -0800

    workflow update and movie table modifications

--- schema-model/Tables/dbo.Movies.sql
+++ schema-model/Tables/dbo.Movies.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Movies]
 [ID] [int] NOT NULL IDENTITY(1, 1),
 [Title] [nvarchar] (50) NOT NULL,
 [Category] [nvarchar] (50) NOT NULL,
-[IMDB] [decimal] (6, 2) NULL
+[IMDB] [decimal] (6, 2) NULL,
+[Language] [nchar] (25) NULL
 )
 GO
