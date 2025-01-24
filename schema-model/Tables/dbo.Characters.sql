ab10b740a164c011a7b7d1c300e50976efef1788
Chris Reyes <Chris.Reyes@red-gate.com>
2025-01-24 13:44:24 -0600

    alter table characters

--- schema-model/Tables/dbo.Characters.sql
+++ schema-model/Tables/dbo.Characters.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[Characters]
 [Name] [nvarchar] (100) NOT NULL,
 [Race] [nvarchar] (50) NULL,
 [Home] [nvarchar] (100) NULL,
-[Weapon] [nvarchar] (50) NULL
+[Weapon] [nvarchar] (50) NULL,
+[Age] [nchar] (10) NULL
 )
 GO
 ALTER TABLE [dbo].[Characters] ADD CONSTRAINT [PK__Characte__757BCA401E18DCD0] PRIMARY KEY CLUSTERED ([CharacterID])
