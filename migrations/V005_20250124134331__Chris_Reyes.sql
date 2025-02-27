930b8bab35c8061d6efbb30c29f2b13fa65adc68
Chris Reyes <Chris.Reyes@red-gate.com>
2025-02-27 10:51:29 -0800

    Commiting migration

--- migrations/V005_20250124134331__Chris_Reyes.sql
+++ migrations/V005_20250124134331__Chris_Reyes.sql
@@ -3,8 +3,11 @@ GO
 SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
 GO
 PRINT N'Altering [dbo].[Characters]'
+
 GO
 ALTER TABLE [dbo].[Characters] ADD
 [Age] [nchar] (10) NULL
 GO
 
+-- select * from tableA, tableB where tableA.field1 = tableB.field1
+-- select * from table A inner join tableB on tableA.field1 = tableB.field1
