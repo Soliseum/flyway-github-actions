9c009e2fe99bd3c3278757e39c8a761461cc0ece
Chris Reyes <Chris.Reyes@red-gate.com>
2025-05-09 13:04:38 -0700

    clents rmv clm

---
+++ migrations/U012_20250509130407__UNDO-Chris_Reyes.sql
@@ -0,0 +1,14 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [dbo].[Clients]'
+GO
+ALTER TABLE [dbo].[Clients] ADD
+[TempColumn] [nchar] (10) NULL
+GO
+PRINT N'Refreshing [dbo].[vw_ClientOverview]'
+GO
+EXEC sp_refreshview N'[dbo].[vw_ClientOverview]'
+GO
+
