a478c972a2cc2c73eacd104ad30b9936e7a80fef
Chris Reyes <Chris.Reyes@red-gate.com>
2025-08-28 10:58:16 -0700

    Alter Table Clients

---
+++ migrations/V015_20250828105633__Alter Table Clients.sql
@@ -0,0 +1,20 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [dbo].[Clients]'
+GO
+IF COL_LENGTH(N'[dbo].[Clients]', N'MMA') IS NULL
+ALTER TABLE [dbo].[Clients] ADD[MMA] [nchar] (10) NULL
+GO
+PRINT N'Refreshing [dbo].[vw_ClientOverview]'
+GO
+IF OBJECT_ID(N'[dbo].[vw_ClientOverview]', 'V') IS NOT NULL
+EXEC sp_refreshview N'[dbo].[vw_ClientOverview]'
+GO
+PRINT N'Refreshing [dbo].[vw_RecentClientActivity]'
+GO
+IF OBJECT_ID(N'[dbo].[vw_RecentClientActivity]', 'V') IS NOT NULL
+EXEC sp_refreshview N'[dbo].[vw_RecentClientActivity]'
+GO
+
